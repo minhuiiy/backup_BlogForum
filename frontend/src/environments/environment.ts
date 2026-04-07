0de9135bb3bef8e5db72f72b885d20063189d02f
NguyenTanSang2210 <tansangnts2004@gmail.com>
2026-04-07 22:53:58 +0700

    Push code lần đầu

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://thanksgiving-files-serial-buy.trycloudflare.com/api/v1',
-  wsUrl: 'https://thanksgiving-files-serial-buy.trycloudflare.com/ws'
-  // wsUrl: 'http://localhost:8080/api/v1'
+  production: false,
+  apiUrl: '/api/v1',
+  wsUrl: 'http://localhost:8080/ws'
 };
